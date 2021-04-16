e0bc8d3938006775217a905c601169ea809a585c
Manuel Wolkowitsch <e1650767@student.tuwien.ac.at>
2021-04-16 18:23:31 +0200

    refactor: refactor manageFeed

--- pages/manageFeed.tsx
+++ pages/manageFeed.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/Link';
-import {useEffect, useState} from 'react';
 import {useRouter} from 'next/router';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPen, faTrash} from '@fortawesome/free-solid-svg-icons';
-import {getFeedItemsFromLocalStorage} from '../util/getFeedItemsFromLocalStorage';
-import {saveFeedItemsToLocalStorage} from '../util/saveFeedItemsToLocalStorage';
 import Dialog from '../components/dialog';
 import {Navigation} from '../components/navigation';
+import storage from '../util/storage';
 
 export default function ManageFeed(): React.ReactElement {
   const [feedItems, setFeedItems] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
-    getFeedItemsFromLocalStorage(setFeedItems);
+    setFeedItems(storage.getItem("feedItems"));
   }, []);
   useEffect(() => {
-    saveFeedItemsToLocalStorage(feedItems);
+    storage.setItem("feedItems", feedItems);
   }, [feedItems]);
-  const deleteHandler = (id) => {
+
+  const deleteFeedItem = (id) => {
     if (window.confirm('Are you sure you wish to delete this item?')) {
       const updatedFeedItems = [...feedItems].filter(
         (feedItem) => feedItem.id !== id
@@ -27,46 +26,22 @@ export default function ManageFeed(): React.ReactElement {
       setFeedItems(updatedFeedItems);
     }
   };
-  const editHandler = (id) => {
-    feedItems.map((feedItem) => {
-      if (feedItem.id === id) {
-        feedItem.edit = true;
-        router.push('/createFeed');
-      }
-    });
-    saveFeedItemsToLocalStorage(feedItems);
+  const editFeedItem = (id) => {
+    feedItems.find(item => item.id === id).edit = true;
+    router.push('/createFeed');
+    storage.setItem("feedItems", feedItems);
   };
 
   return (
     <Dialog>
       <Navigation/>
       <main className="flex flex-col items-center h-full">
-        <p className="m-2">currently no RSS - Feed</p>
-        {feedItems.map((feedItem) => (
-          <div
-            className="flex flex-row items-center justify-evenly w-full my-2 border-b"
-            key={feedItem.id}
-          >
-            <img className="max-w-8 max-h-8" src={feedItem.icon}/>
-            <div
-              className={!feedItem.status ? 'text-gray-400 font-light' : ''}
-            >
-              {feedItem.url.substring(0, 20)}
-            </div>
-            <FontAwesomeIcon
-              icon={faPen}
-              onClick={() => editHandler(feedItem.id)}
-            />
-            <FontAwesomeIcon
-              icon={faTrash}
-              onClick={() => deleteHandler(feedItem.id)}
-            />
-          </div>
-        ))}
+        {feedItems.length === 0 ? <div className="m-2">currently no RSS - Feed</div> : null}
+        {feedItems.map((feedItem) => FeedItem(feedItem, editFeedItem, deleteFeedItem))}
         {feedItems.length < 3 ? (
           <Link href="/createFeed">
             <button
-              className="bg-gray-200 hover:bg-blue-700 hover:text-white text-black font-bold py-2 px-4 rounded-3xl w-28">
+              className="bg-gray-200 hover:bg-blue-700 hover:text-white text-black font-bold py-2 px-4 rounded-3xl">
               + add Feed
             </button>
           </Link>
@@ -74,4 +49,26 @@ export default function ManageFeed(): React.ReactElement {
       </main>
     </Dialog>
   );
+}
+
+function FeedItem(feedItem, editFeedItem: (id) => void, deleteFeedItem: (id) => void) {
+  return <div
+    className="flex flex-row items-center w-full my-2 border-b"
+    key={feedItem.id}
+  >
+    <img className="max-w-8 max-h-8 mx-2" src={feedItem.icon}/>
+    <div className={`${!feedItem.status ? 'text-gray-400 font-light' : ''} w-full truncate`}>
+      {feedItem.url}
+    </div>
+    <FontAwesomeIcon
+      className="mx-2 cursor-pointer"
+      icon={faPen}
+      onClick={() => editFeedItem(feedItem.id)}
+    />
+    <FontAwesomeIcon
+      className="mx-2 cursor-pointer"
+      icon={faTrash}
+      onClick={() => deleteFeedItem(feedItem.id)}
+    />
+  </div>;
 }
