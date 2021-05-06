b210ba2e40e387c38c8473871eb63d119e4d8021
Manuel Wolkowitsch <e1650767@student.tuwien.ac.at>
2021-05-06 16:29:21 +0200

    chore: change database host

--- util/globals.ts
+++ util/globals.ts
@@ -2,7 +2,7 @@ export default {
   host: "http://localhost:3000",
   openid_host: "https://waecm-sso.inso.tuwien.ac.at/auth/realms/waecm/protocol/openid-connect",
   openid_clientid: "waecm",
-  database_path: "mongodb://localhost:27017/waecm2021group3db",
+  database_path: "mongodb://database:27017/waecm2021group3db",
   database_user: "service-user",
   database_password: "itsedev",
   timerRunning: false
